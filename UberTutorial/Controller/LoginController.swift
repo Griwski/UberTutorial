0b70a94b0da76e232aa8f1da9c088f24bee7fea0
griwski <griwski@gmail.com>
2020-12-03 15:59:05 +0300

    project setup

---
+++ UberTutorial/Controller/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  UberTutorial
+//
+//  Created by Mikhail Artsiukh on 12/1/20.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
